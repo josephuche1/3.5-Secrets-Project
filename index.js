da8d9ebef8e2f88893119e58de947356a1e045fd
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-21 08:37:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyPrarser from "body-parser";
+import morgan from "morgan";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
