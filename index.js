f8dc54cffeafe495405d1239bbcccf0eef5c8eba
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-21 09:19:34 +0300

    added code to show user the secret page if the password is correct, and to refresh if the password is wrong

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
 import express from "express";
-import bodyPrarser from "body-parser";
+import bodyParser from "body-parser";
 import morgan from "morgan";
 import {dirname} from "path";
 import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+let port = 3000;
+let expected = "ILoveProgramming"
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(morgan("tiny"));
+
+app.post("/check", (req,res) => {
+    if(req.body["password"] == expected){
+        console.log("success")
+        res.sendFile(__dirname+"/public/secret.html");
+    }
+    else{
+        console.log("fail");
+        res.redirect("/");
+    }
+})
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname+"/public/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to server ${port}`);
+})
